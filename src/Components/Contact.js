88d6adb0b0a3abf3d6cd91e5183afbfe966c81fe
phani-atluri <phaniatluri07@gmail.com>
2020-10-15 23:42:15 -0400

    contact component updated

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,13 +4,9 @@ import SendIcon from "@material-ui/icons/Send";
 function Contact() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
+
   const [message, setMessage] = useState("");
 
-  var handleclick = (e) => {
-    e.preventDefault();
-    window.open(`mailto:${email}?subject=${subject}&body=${name}: ${message}`);
-  };
   return (
     <section className="contact" id="contact">
       <div className="contact__main">
@@ -18,46 +14,40 @@ function Contact() {
           Thank you for visiting, I hope you like it.If you have any suggestions
           or to connect. please send me a message .
         </h3>
-        <div className="contact__form">
-          <label>
-            <input
-              type="text"
-              className="inpt"
-              placeholder="name"
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-            />
-          </label>
-          <label>
-            <input
-              type="email"
-              className="inpt"
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="email"
-              value={email}
-            />
-          </label>
-          <label>
-            <input
-              type="text"
-              className="inpt"
-              onChange={(e) => setSubject(e.target.value)}
-              placeholder="Subject"
-              value={subject}
-            />
-          </label>
-          <label>
-            <textarea
-              placeholder="Message"
-              onChange={(e) => setMessage(e.target.value)}
-              value={message}
-            />
-          </label>
-          <button onClick={handleclick}>
-            <span>SEND</span>
-            <SendIcon />
-          </button>
-        </div>
+        <form name="contactForm" method="post" netlify>
+          <div className="contact__form">
+            <label>
+              <input
+                type="text"
+                className="inpt"
+                placeholder="name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+            </label>
+            <label>
+              <input
+                type="email"
+                className="inpt"
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="email"
+                value={email}
+              />
+            </label>
+
+            <label>
+              <textarea
+                placeholder="Message"
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+              />
+            </label>
+            <button>
+              <span>SEND</span>
+              <SendIcon />
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   );
